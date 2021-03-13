a72f905e76deb879d2f9efd52ff938c2c2d248ad
Marcelino Rodrigues <marcelino.rodrigues.d.n@gmail.com>
2021-03-12 22:09:07 -0300

    initial

---
+++ Myka/Data/AplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Myka.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Myka.Data
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
